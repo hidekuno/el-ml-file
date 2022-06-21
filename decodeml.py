3ba787ccf1c0ac07ae17c77e8ccee9fc61a0c381
hideki kuno <hidekuno@gmail.com>
2022-06-21 18:42:02 +0900

    https://lists.ubuntu.com/archives/ubuntu-jp.mbox/ubuntu-jp.mboxに対応した

---
+++ decodeml.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import sys,email
+from email.header import decode_header
+from email.utils import parsedate_to_datetime
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-f', dest='filename', type=str, required=True)
+args = arg_parser.parse_args(sys.argv[1:])
+
+with open(args.filename, 'rb') as fd:
+    msg = email.message_from_bytes(fd.read())
+
+    for k in msg.keys():
+        header = ''
+        for tup in decode_header(str(msg[k])):
+            if type(tup[0]) is bytes:
+                charset = tup[1]
+                if charset:
+                    header += tup[0].decode(tup[1])
+                else:
+                    header += tup[0].decode()
+            elif type(tup[0]) is str:
+                header += tup[0]
+        print(k + ':', header)
+
+    print('')
+    for part in msg.walk():
+        if part.get_content_maintype() == "multipart":
+            continue
+        if not part.get_filename():
+            charset = part.get_content_charset()
+            if charset:
+                print(part.get_payload(decode=True).decode(charset, errors="replace"))
+            else:
+                print(part.get_payload(decode=True).decode())
