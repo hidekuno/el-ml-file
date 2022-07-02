8177cee170f941fff0d1956980d779a02ac4546c
hidekuno <hidekuno@gmail.com>
2022-07-02 20:37:24 +0900

    macosではgnu-sedを使用するように修正した

--- create_idx.sh
+++ create_idx.sh
@@ -3,17 +3,21 @@
 egrep -r -m 3 -i "^(Subject|Message-Id|Date):" . |tr -d '\015'|sort > idx1
 egrep -r -m 2 -i "^(In-Reply-to|References):" .  |tr -d '\015'|sort > idx2
 
+if [ `uname` = 'Darwin' -a -x /usr/local/bin/gsed ]; then
+  SED=/usr/local/bin/gsed
+else
+  SED=/usr/bin/sed
+fi
+
 # maillinkg list was corrected To make the ml-file work
-sed -i.bak -e 's/Date:\(.*\)jst/Date:\1JST/' \
-            -e 's/+900/+0900/' \
-            -e 's/+0900(JST)/+0900 (JST)/' \
-            -e 's/          U\*/ U*/' idx1
+$SED -i -e 's/Date:\(.*\)jst/Date:\1JST/' \
+        -e 's/+900/+0900/' \
+        -e 's/+0900(JST)/+0900 (JST)/' \
+        -e 's/          U\*/ U*/' idx1
 
-sed -i.bak -e 's/In-Reply-To:\(.*\)jst/In-Reply-To:\1JST/' \
-            -e 's/References: .*message of.*</References: </' \
-            -e 's/<Your.*</</' -e 's/> Your.*$//' -e 's/></> </g' \
-            -e 's/In-Reply-To: .*<\(.*\)>.*$/In-Reply-To: <\1>/i' \
-            -e 's/          U\*/ U*/' \
-            -e '/In-Reply-To: $/d' idx2
-
-rm -f *.bak
+$SED -i -e 's/In-Reply-To:\(.*\)jst/In-Reply-To:\1JST/' \
+        -e 's/References: .*message of.*</References: </' \
+        -e 's/<Your.*</</' -e 's/> Your.*$//' -e 's/></> </g' \
+        -e 's/In-Reply-To: .*<\(.*\)>.*$/In-Reply-To: <\1>/i' \
+        -e 's/          U\*/ U*/' \
+        -e '/In-Reply-To: $/d' idx2
