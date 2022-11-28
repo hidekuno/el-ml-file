2ee1b39726c01e8e196431b68879a672af206e3c
hideki kuno <hidekuno@gmail.com>
2022-11-28 14:14:55 +0900

    初期表示失敗のエラーメッセージをminibuffeに表示するように修正した

--- treeml.py
+++ treeml.py
@@ -56,54 +56,53 @@ def makeMessages():
     dates = {}
     messages = {}
 
-    fd = open('idx1','r')
-    for line in fd:
-        line = line.rstrip()
-        rec = line.split(':')
-        if not rec[0] in mails:
-            mails[rec[0]] = Message(rec[0])
+    with open('idx1','r') as fd:
+        for line in fd:
+            line = line.rstrip()
+            rec = line.split(':')
+            if not rec[0] in mails:
+                mails[rec[0]] = Message(rec[0])
 
-        ml = mails[rec[0]]
-        if rec[1].lower() == "date":
-            ml.date = adjust_jst(line.split(":Date: ")[1])
-            dates[ml.date] = ml
-        if rec[1].lower() == "message-id":
-            ml.messageId = " ".join(line.split(" ")[1:])
-            messages[ml.messageId] = ml
-        if rec[1].lower() == "subject":
-            ml.subject = line.split(":Subject: ")[1]
-    fd.close()
+            ml = mails[rec[0]]
+            if rec[1].lower() == "date":
+                ml.date = adjust_jst(line.split(":Date: ")[1])
+                dates[ml.date] = ml
+            if rec[1].lower() == "message-id":
+                ml.messageId = " ".join(line.split(" ")[1:])
+                messages[ml.messageId] = ml
+            if rec[1].lower() == "subject":
+                ml.subject = line.split(":Subject: ")[1]
+
     return dates,messages
 
 def makeRef(dates,messages):
-    fd = open('idx2','r')
-    for line in fd:
-        line = line.rstrip()
-        rec = line.split(':')
-        filename = rec[0]
+    with open('idx2','r') as fd:
+        for line in fd:
+            line = line.rstrip()
+            rec = line.split(':')
+            filename = rec[0]
 
-        if rec[1].lower() == "in-reply-to":
-            irt = line.split(' ')[1:]
+            if rec[1].lower() == "in-reply-to":
+                irt = line.split(' ')[1:]
 
-            if irt[0][0] == '<' and irt[-1][-1] == '>':
-                set_ref(" ".join(irt[0:]),messages,filename)
+                if irt[0][0] == '<' and irt[-1][-1] == '>':
+                    set_ref(" ".join(irt[0:]),messages,filename)
 
-            elif irt[0] == 'Your' and irt[1] == 'message':
-                set_ref(adjust_jst(" ".join(irt[3:]).replace('.', '').replace('"','')), dates, filename)
+                elif irt[0] == 'Your' and irt[1] == 'message':
+                    set_ref(adjust_jst(" ".join(irt[3:]).replace('.', '').replace('"','')), dates, filename)
 
-            elif 5 < len(irt) and irt[2] == 'message' and irt[3] == 'of':
-                set_ref(adjust_jst(" ".join(irt[4:]).replace('.', '').replace('"','')), dates, filename)
+                elif 5 < len(irt) and irt[2] == 'message' and irt[3] == 'of':
+                    set_ref(adjust_jst(" ".join(irt[4:]).replace('.', '').replace('"','')), dates, filename)
 
-        if rec[1].lower() == "references":
-            ref = line.split(' ')[1:]
+            if rec[1].lower() == "references":
+                ref = line.split(' ')[1:]
 
-            if len(ref) < 1 or ref[0] == 'Your' or ',' in line:
-                pass
-            elif ref[-1][0] == '<':
-                set_ref(ref[-1],messages,filename)
-            elif ref[-1][-1] == '>':
-                set_ref(" ".join(ref[-2:]),messages,filename)
-    fd.close()
+                if len(ref) < 1 or ref[0] == 'Your' or ',' in line:
+                    pass
+                elif ref[-1][0] == '<':
+                    set_ref(ref[-1],messages,filename)
+                elif ref[-1][-1] == '>':
+                    set_ref(" ".join(ref[-2:]),messages,filename)
 
 # create tree recursive
 def create_tree(rec,top):
@@ -126,12 +125,16 @@ if __name__ == "__main__":
     arg_parser.add_argument('ml', type=str)
     args = arg_parser.parse_args(sys.argv[1:])
 
-    os.chdir(os.path.join(os.environ['HOME'], args.ml))
+    try:
+        os.chdir(os.path.join(os.environ['HOME'], args.ml))
 
-    dates, messages = makeMessages()
-    makeRef(dates,messages)
-    top = Tree(Message(args.ml))
+        dates, messages = makeMessages()
+        makeRef(dates,messages)
+        top = Tree(Message(args.ml))
 
-    for k in mails.keys():
-        create_tree(mails[k],top)
-    walk(top)
+        for k in mails.keys():
+            create_tree(mails[k],top)
+        walk(top)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
